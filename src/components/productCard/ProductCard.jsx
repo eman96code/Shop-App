2531bbdcd393acc6fc9953f8aec25bd410161d58
eman96code <eman96abdelmawla@gmail.com>
2023-11-28 04:03:31 +0200

    adding productCard component

---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,144 @@
+import PropTypes from 'prop-types';
+const ProductCard = (props) => {
+  const { product } = props;
+  return (
+    <div className="w-80 bg-white shadow rounded">
+      <div
+        className="h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center"
+        style={{
+          background: `url(${product.thumbnail})`,
+        }}
+      >
+        <div className="flex justify-between">
+          <input type="checkbox" />
+          <button className="text-white hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </button>
+        </div>
+        <div>
+          <span className="uppercase text-xs bg-green-50 p-0.5 border-green-500 border rounded text-green-700 font-medium select-none">
+            available
+          </span>
+        </div>
+      </div>
+      <div className="p-4 flex flex-col items-center">
+        <h1
+          className="text-gray-800 text-center mt-1 whitespace-no-wrap overflow-hidden overflow-ellipsis line-clamp-1"
+          style={{ maxWidth: '100%' }}
+        >
+          {product.title}
+        </h1>
+        <p
+          className="text-gray-400 font-light text-xs text-center text-gray-700 leading-6 whitespace-no-wrap overflow-hidden overflow-ellipsis line-clamp-1"
+          style={{ maxWidth: '100%' }}
+        >
+          {product.description}
+        </p>
+        <div className="price flex  space-x-5 items-center">
+          <p className="oldPrice text-center text-gray-800 mt-1 line-through">{`${product.price} LE`}</p>
+          <p className="newPrice text-center text-gray-800 mt-1">
+            {`${(
+              product.price -
+              (product.price / 100) * product.discountPercentage
+            ).toFixed(2)} LE`}
+          </p>
+        </div>
+
+        <div className="inline-flex items-center mt-2">
+          <button className="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M20 12H4"
+              />
+            </svg>
+          </button>
+          <div className="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none">
+            2
+          </div>
+          <button className="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
+          </button>
+        </div>
+        <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center">
+          more details
+        </button>
+        <div className="flex justify-between w-full mt-4 items-center">
+          <div >
+            <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50  w-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 ml-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+            </button>
+          </div>
+          <div>
+            <button className="py-2 px-4 bg-gray-100 text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                stroke="none"
+                className="h-6 w-6"
+                onClick={(e) => {
+                  e.currentTarget.style.color = 'red';
+                }}
+              >
+                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C16.09 3.81 17.76 3 19.5 3 22.58 3 25 5.42 25 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+ProductCard.propTypes = {
+  product: PropTypes.object,
+};
+
+export default ProductCard;
