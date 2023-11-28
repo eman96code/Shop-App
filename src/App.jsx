fa190823c25864dbd7822fa5ece15f51caf622a7
eman96code <eman96abdelmawla@gmail.com>
2023-11-28 04:07:42 +0200

    updating project structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import NavBar from './components/navBar/NavBar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import  Home  from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
+import Cart from './components/cart/Cart.jsx';
+import ProductDetails from './components/productDetails/ProductDetails.jsx';
+
+function App() {
+  return (
+    <div className="content-page">
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='home/:productId' element= {<ProductDetails/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
